13ecd79b843d466cf67aca3f3baaa72acb91af12
gromandre <gromov7397@yandex.ru>
2021-12-17 10:20:48 +0300

    1_version

---
+++ 5/5/005_authors_and_books.php
@@ -0,0 +1,28 @@
+<?php
+
+$library = [
+    'authors' => [
+        [
+            'name' => 'Джон Маккормик',
+            'email' => 'john_makkormik@example.com',
+        ],
+        [
+            'name' => 'Мартин Роберт',
+            'email' => 'martin_robert@example.com',
+        ],
+    ],
+    'books' => [
+        [
+            'title' => 'Чистый код: создание, анализ и рефакторинг',
+            'author' => 'martin_robert@example.com',
+        ],
+        [
+            'title' => 'Девять алгоритмов, которые изменили будущее',
+            'author' => 'john_makkormik@example.com',
+        ],
+        [
+            'title' => 'Идеальный программист',
+            'author' => 'martin_robert@example.com',
+        ],
+    ],
+];
